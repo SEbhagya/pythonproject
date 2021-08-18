29fcfbd95ff4b596d8b89e5c12ef7cd29326591a
SEbhagya <89145832+SEbhagya@users.noreply.github.com>
2021-08-18 20:58:00 +0530

    Add files via upload
    trial commit change

---
+++ python_string manipulation_july_29.py
@@ -0,0 +1,127 @@
+##my_caption = "dhoni"
+##year = 2021
+##my_statement = "csk was captioned by" + my_caption + "during" + year
+##print(my_statement)
+
+##my_statement = "csk was captioned by" + my_caption + "during" + str(year)
+##print(my_statement)
+##typecasting
+
+##my_statement = "csk was captioned by %s during %d"%(my_caption,year) 
+##print(my_statement)
+##
+##my_statement = "csk was captioned by {} during {}".format(my_caption,year)
+##print(my_statement)
+
+##my_statement = f"csk was captioned by {my_caption} during {year}"
+##print(my_statement)
+
+
+
+##string slicing-- happens in forward direction
+
+##my_data = "india history"
+##print(my_data)
+##print(my_data[1])
+##print(my_data[1:])
+##print(my_data[1:7])
+##print(my_data[5:1])
+
+
+
+##my_data = "dhoni"
+##d = 0
+##h = 1 , -4
+##o = 2 , -3
+##n = 3 , -2
+##i = 4 , -1
+##print(my_data[-1])
+##print(my_data[-2:])
+##print(my_data[-3:4])
+##print(my_data[1:-1])
+##print(my_data[1:-2])
+
+##my_data = "dhonimsd"
+##d = 0
+##h = 1 , -4
+##o = 2 , -3
+##n = 3 , -2
+##i = 4 , -1
+##print(my_data[::2])
+##print(my_data[::1])
+##print(my_data[::-2])
+##print(my_data[::-1])  ##systax for reversing the given dara
+
+
+
+##
+##my_data = "dhoni msd"
+##print(len(my_data))
+##print(type(my_data))
+##print(dir(my_data))
+##
+##my_string = "india is history"
+##print(my_string.count("s"))
+##print(my_string.count("s",4))
+##print(my_string.count("s",2,9))
+
+ 
+##my_string = "india is history"
+##print(my_string.find("i"))
+##print(my_string.find("i",2))
+##print(my_string.find("i",4,10))
+
+
+##
+##my_string = "india is history"
+##print(my_string.index("i"))
+##print(my_string.index("i",2))
+##print(my_string.index("i",2,5))
+
+
+##
+##my_string = "india is history"
+##print(my_string.rfind("i"))
+##r= rear find  from backwards
+
+
+##
+##my_string = "india is history"
+##print(my_string.rindex("i"))
+
+
+##my_string = "india is history"
+##print(my_string.rindex("z"))
+##print("welcome")
+
+
+##my_string = "india is history"
+##print(my_string.find("z"))
+##print("welcome")
+
+##
+##my_string = "india is history"
+##my_output = list(my_string)
+##print(my_output)
+##print(type(my_output))
+
+##
+##
+##my_output = my_string.split()
+##print(my_output)
+##print(type(my_output))
+
+##
+##string = "hello world"
+##print(string.replace("hello", "hi"))
+##print(string.replace("l","d"))
+
+
+
+
+
+
+
+
+
+
